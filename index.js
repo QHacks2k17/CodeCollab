7d130a2de6a3ed8f1cebe69bca1f1fb20500c1f4
Tolu Olubode <toluwanimiolubode@gmail.com>
2017-02-04 02:13:44 -0500

    trudging along

--- index.js
+++ index.js
@@ -1,69 +1,41 @@
-var port = 8080;
-var serverUrl = "127.0.0.1";
+var http = require('http');
+var fs = require('fs');
+var filePath = req.url;
+if (filePath == '/')
+    filePath = '/index.html';
 
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
-var checkMimeType = true;
+filePath = __dirname + filePath;
+var extname = path.extname(filePath);
+var contentType = 'text/html';
 
-console.log("Starting web server at " + serverUrl + ":" + port);
-
-http.createServer( function(req, res) {
-
-	var now = new Date();
-
-	var filename = req.url || "index.html";
-	var ext = path.extname(filename);
-	var localPath = __dirname;
-	var validExtensions = {
-		".html" : "text/html",
-		".js": "application/javascript",
-		".css": "text/css",
-		".txt": "text/plain",
-		".jpg": "image/jpeg",
-		".gif": "image/gif",
-		".png": "image/png",
-		".woff": "application/font-woff",
-		".woff2": "application/font-woff2"
-	};
-
-	var validMimeType = true;
-	var mimeType = validExtensions[ext];
-	if (checkMimeType) {
-		validMimeType = validExtensions[ext] != undefined;
-	}
-
-	if (validMimeType) {
-		localPath += filename;
-		fs.exists(localPath, function(exists) {
-			if(exists) {
-				console.log("Serving file: " + localPath);
-				getFile(localPath, res, mimeType);
-			} else {
-				console.log("File not found: " + localPath);
-				res.writeHead(404);
-				res.end();
-			}
-		});
-
-	} else {
-		console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
-	}
-
-}).listen(port, serverUrl);
-
-function getFile(localPath, res, mimeType) {
-	fs.readFile(localPath, function(err, contents) {
-		if(!err) {
-			res.setHeader("Content-Length", contents.length);
-			if (mimeType != undefined) {
-				res.setHeader("Content-Type", mimeType);
-			}
-			res.statusCode = 200;
-			res.end(contents);
-		} else {
-			res.writeHead(500);
-			res.end();
-		}
-	});
+switch (extname) {
+case '.js':
+    contentType = 'text/javascript';
+    break;
+case '.css':
+    contentType = 'text/css';
+    break;
 }
+
+fs.exists(filePath, function (exists) {
+
+            if (exists) {
+                fs.readFile(filePath, function (error, content) {
+                    if (error) {
+                        res.writeHead(500);
+                        res.end();
+                    } else {
+                        res.writeHead(200, {
+                            'Content-Type': contentType
+                        });
+                        res.end(content, 'utf-8');
+                    }
+                });
+                http.createServer(function (request, response) {
+                    response.writeHeader(200, {
+                        "Content-Type": "text/html"
+                    }); // <-- HERE!
+                    response.write(html); // <-- HERE!
+                    response.end();
+                }).listen(1337, '127.0.0.1');
+            }
